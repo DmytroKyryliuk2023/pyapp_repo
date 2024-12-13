b186c9ff900cad510b2ab9f2916b0849d6df09db
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2024-12-13 04:18:04 +0200

    list union

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import random
 
 def main():
-    thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
-    print(thistuple[-4:])
+    list1 = [random.randint(1, 10) for i in range(10)]
+    list2 = [random.randint(1, 10) for i in range(10)]
+    list3 = list1 + list2
+    print(list3)
 
 if __name__ == "__main__":
     main()
