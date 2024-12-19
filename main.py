7bd3e0be9771d38f0bd1e0db3b863bd32a77cf9d
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2024-12-19 21:43:01 +0200

    subjects

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-# import random
+from typing import Type
 
-LIST = [1, 3, 4, 5, 6]
 
-def insert(pos: int, value) -> None:
-    LIST.append(None)
-    n = len(LIST)
+class Subject:
+    def __init__(self, name: str, credits: int):
+        self.name = name
+        self.credits = credits
 
-    # rotate the elements to the right
-    for i in range(n - 1, pos, -1):
-        LIST[i] = LIST[i - 1]
+    def printItem(self):
+        print(f"Name: {self.name}, Credits: {self.credits}", end="")
 
-    LIST[pos] = value
+class SubjectExpand(Subject):
+    def __init__(self, name: str, credits: int, teacher: str):
+        super().__init__(name, credits)
+        self.teacher = teacher
+
+    def printItem(self):
+        super().printItem()
+        print(f", Teacher: {self.teacher}", end="")
+
+
+class Database:
+    def __init__(self, items):
+        self.items: list[Subject] = list(items)
+
+    def add(self, item: Subject):
+        self.items.append(item)
+
+    def printItems(self):
+        for item in self.items:
+            item.printItem()
+            print()
 
 def main():
-    print(LIST)
-    insert(1, 2)
-    print(LIST)
-    LIST.pop()
-    print(LIST)
+    item1 = Subject("Math", 6)
+    item2 = SubjectExpand("OS", 5, "Pleskanka")
 
+    database = Database((item2,))
+
+    database.add(item1)
+    database.add(item2)
+
+    database.printItems()
 
 if __name__ == "__main__":
     main()
