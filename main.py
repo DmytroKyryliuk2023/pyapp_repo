578f9e5802bf422342d27768a5a5e7aac1e00e65
DmytroKyryliuk2023 <dmitrokiriluk1208@gmail.com>
2024-12-21 04:56:46 +0200

    end of the last project

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
-# import random
-
-LIST = [1, 3, 4, 5, 6]
-
-def insert(pos: int, value) -> None:
-    LIST.append(None)
-    n = len(LIST)
-
-    # rotate the elements to the right
-    for i in range(n - 1, pos, -1):
-        LIST[i] = LIST[i - 1]
-
-    LIST[pos] = value
+import proba as p
+import platform
 
 def main():
-    print(LIST)
-    insert(1, 2)
-    print(LIST)
-    LIST.pop()
-    print(LIST)
-
+    x = dir(platform)
+    x = dir(p)
+    print(x)
 
 if __name__ == "__main__":
     main()
